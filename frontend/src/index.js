13b2c94f63a21aabd8f2557f3492fdc7b78c30da
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-06-16 11:10:15 -0700

    main app render app to root element

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import * as serviceWorker from "./serviceWorker";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <App/>
+  </React.StrictMode>,  
+  document.getElementById('root')
 );
